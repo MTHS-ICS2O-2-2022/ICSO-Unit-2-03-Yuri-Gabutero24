1e754d9aa469ab857f96b6697e879714db215908
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-03-20 17:11:37 +0000

    webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Yuri G
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+/**
+ * This function gets the users Street Address.
+ */
+function enterClicked() {
+  // input
+  const streetName = document.getElementById('street-name').value;
+  const streetNumber = document.getElementById('street-number').value;
+
+  //output
+  document.getElementById("address").innerHTML = "Your address is: " + streetNumber + ", " + streetName ;
+}
